63e50e71a5811820925df51da3dfbabc8bf2c43d
Nishar <mdnishar75640@gmail.com>
2026-05-09 21:32:15 +0530

    Add files via upload

---
+++ DSA_JS/Array/sum.js
@@ -0,0 +1,12 @@
+let arr = [10,20,30,40];
+
+function sum(arr){
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i ++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+console.log(sum(arr));
