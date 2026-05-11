493c41102bd5beb739bd4194586cd0770934eb63
Nishar <mdnishar75640@gmail.com>
2026-05-11 23:12:07 +0530

    Add files via upload

---
+++ DSA_JS/Array/even.js
@@ -0,0 +1,33 @@
+// let arr = [10, 20, 30, 40];
+
+// function countEven(arr){
+//     let count = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] %2 === 0){
+//         count ++
+//     }
+//     }
+//     return count;
+// }
+
+// console.log(countEven(arr));
+
+let nums = [2,5,8,6]
+
+function findMissingNumber(nums) {
+
+    let n = nums.length;
+
+    // expected sum
+    let expected = (n * (n + 1)) / 2;
+
+    // actual sum
+    let actual = 0;
+    for (let i = 0; i < n; i++) {
+        actual += nums[i];
+    }
+
+    return expected - actual;
+}
+
+console.log(findMissingNumber(nums));
